fedd061e05878b80656459d43774024bf3fd987f
R10D10 <genius@genius.com>
2023-12-01 19:28:23 +0100

    part 2 ok

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -4,7 +4,7 @@ with open('input.txt', 'r') as file:
     data = file.read().splitlines()
 
 
-def part_one(result: int = 0) -> int:
+def part_one(data:list, result: int = 0) -> int:
     for line in data:
         new_line: str = re.sub(r'[A-z]', '', line)
         number_found: str = new_line[0] + new_line[-1]
@@ -12,5 +12,22 @@ def part_one(result: int = 0) -> int:
     return result
 
 
-digit_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+digit_dict = {"one": "o1e",
+              "two": "t2o",
+              "three": "t3e",
+              "four": "f4r",
+              "five": "f5e",
+              "six": "s6x",
+              "seven": "s7n",
+              "eight": "e8t",
+              "nine": "n9e"}
 
+f = open("input.txt", "r")
+text = f.read()
+
+for key, value in digit_dict.items():
+    text = text.replace(key, value)
+
+text_in_line = text.splitlines()
+
+print(part_one(text_in_line))
