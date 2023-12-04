7e6cc70611479d7c113995da892282ef918888cb
HX <kidztse@gmail.com>
2023-12-04 21:23:09 +0100

    Learnt from it today, need to review later

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,24 +1,34 @@
-from collections import defaultdict
-import math
 
 with open('input.txt', 'r') as file:
     lottery = file.read().splitlines()
 
 result = 0
 found = 0
+tracking_map = {}
+
 for index, line in enumerate(lottery):
+    if index not in tracking_map:
+        tracking_map[index] = 1
+
     D = line.split(":")[1]
     numbers_picked, numbers_to_check = [list(map(int, i.split())) for i in D.split("|")]
 
     # True + True = 2
     found = sum(number in numbers_picked for number in numbers_to_check)
 
-    if found > 0:
-        result += 2 ** (found-1)
-
-print(result)
+    for next_card in range(index + 1, index + found + 1):  # card 1 in range(1, 5)
+        tracking_map[next_card] = tracking_map.get(next_card, 1) + tracking_map[index]  # adding the times we got the card
 
 
+print(sum(tracking_map.values()))
+
+
+
+# What learnt today:
+# 1. we can use map to wash string to int
+# 2. note True + True = 2, we can use to find a number existing in both side
+# 3. need to use more list comprehension
+# 4. why tracking_map[index] is the times we got the card?
 
 
 
