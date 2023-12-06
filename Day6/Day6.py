ec933a48787b4973c5fe03c66a1d12edd3787dc2
HX <kidztse@gmail.com>
2023-12-06 19:14:37 +0100

    part 2 ok

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -7,23 +7,47 @@ arr = []
 for line in ts:
     arr.append(re.findall(r'\d+', line))
 
-t = map(int, arr[0])
-s = map(int, arr[1])
+t = arr[0]
+d = arr[1]
 
-arr = list(zip(t, s))
+time = str()
+distance = str()
+for i in t:
+    time += i
+
+for i in d:
+    distance += i
+
+time, distance = (int(time), int(distance))
+
+count = 0
+for v in range(time + 1):
+    if (time - v) * v > distance:
+        count += 1
+print(count)
 
 
-res = []
-for time, distance in arr:
-    count = 0
-    for v in range(time+1):
+#############################################
+#                 part 1                    #
+#############################################
 
-        if (time - v) * v > distance:
-            count += 1
-    res.append(count)
-
-multi = 1
-for nb in res:
-    multi *= nb
-
-print(multi)
+# t = map(int, arr[0])
+# s = map(int, arr[1])
+#
+# arr = list(zip(t, s))
+#
+#
+# res = []
+# for time, distance in arr:
+#     count = 0
+#     for v in range(time+1):
+#
+#         if (time - v) * v > distance:
+#             count += 1
+#     res.append(count)
+# print(res)
+# multi = 1
+# for nb in res:
+#     multi *= nb
+#
+# print(multi)
